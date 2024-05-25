359b13c340b869a948a3dea81556ee9547bcd0ed
penspanic <penspanic@gmail.com>
2024-05-25 11:53:22 +0900

    스펙 몇개 추가

--- src/Messages.py
+++ src/Messages.py
@@ -1,10 +1,12 @@
 import datetime
+import re
 
 class Message:
     def __init__(self, sender: str, datetime: datetime.datetime, messageLines: str):
         self.sender = sender
         self.datetime = datetime
         self.messageLines = messageLines
+        self.giftMessages = {} # sender, message[]
     
     def getMessage(self):
         return '\n'.join(self.messageLines)
@@ -27,8 +29,27 @@ class MessageHistory:
             self.messagesByDate[dt] = []
         self.messagesByDate[dt].append(message)
 
+        if MessageUtil.IsGift(message):
+            if message.sender not in self.messagesByDate:
+                self.messagesByDate[message.sender] = []
+            self.messagesByDate[message.sender].append(message)
+
     def __str__(self):
         return '\n'.join([str(message) for message in self.messages])
 
     def __repr__(self):
         return '\n'.join([str(message) for message in self.messages])
+
+
+class MessageUtil:
+    giftPatterns = [
+        re.compile("선물과 메세지를 보냈습니다."),
+        re.compile("선물을 보냈습니다.")
+    ]
+
+    @staticmethod
+    def IsGift(message: Message):
+        for pattern in MessageUtil.giftPatterns:
+            if pattern.match(message.getMessage()):
+                return True
+        return False
