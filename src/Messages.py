9ae9641fd9cf05939591a895ef07b8e1fd007b9d
penspanic <penspanic@gmail.com>
2024-05-28 20:24:56 +0900

    이모티콘과 선물 메세지 분석 기능 추가

--- src/Messages.py
+++ src/Messages.py
@@ -1,12 +1,16 @@
 import datetime
 import re
 
+class MessageAttributes:
+    gift = 'gift'
+    emoticon = 'emoticon'
+
 class Message:
     def __init__(self, sender: str, datetime: datetime.datetime, messageLines: str):
         self.sender = sender
         self.datetime = datetime
         self.messageLines = messageLines
-        self.giftMessages = {} # sender, message[]
+        self.attributes = set()
         self.replyTerm = None # datetime.timedelta
     
     def getMessage(self):
@@ -50,9 +54,11 @@ class MessageHistory:
         self.lastReplyTimeBySender[message.sender] = message.datetime
 
         if MessageUtil.IsGift(message):
-            if message.sender not in self.messagesByDate:
-                self.messagesByDate[message.sender] = []
-            self.messagesByDate[message.sender].append(message)
+            message.attributes.add(MessageAttributes.gift)
+
+        if MessageUtil.IsEmoticon(message):
+            message.attributes.add(MessageAttributes.emoticon)
+        
 
     def __str__(self):
         return '\n'.join([str(message) for message in self.messages])
@@ -67,9 +73,15 @@ class MessageUtil:
         re.compile("선물을 보냈습니다.")
     ]
 
+    emoticonPattern = re.compile("이모티콘\s*$")
+
     @staticmethod
     def IsGift(message: Message):
         for pattern in MessageUtil.giftPatterns:
             if pattern.match(message.getMessage()):
                 return True
         return False
+    
+    @staticmethod
+    def IsEmoticon(message: Message):
+        return len(message.messageLines) == 1 and MessageUtil.emoticonPattern.match(message.messageLines[0]) is not None
